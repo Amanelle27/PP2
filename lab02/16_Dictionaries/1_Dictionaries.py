abe83e2627ef9a2874e7a15201095aa0e80bb955
Amanelle27 <amanellecorp@gmail.com>
2025-01-26 01:52:06 +0500

    Added the Dictionaries

---
+++ lab02/16_Dictionaries/1_Dictionaries.py
@@ -0,0 +1,8 @@
+#dictionary is a collection which is ordered*, changeable and do not allow duplicates
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(thisdict)
