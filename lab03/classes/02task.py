d5015ab879682fa66a681a3ae761991035ecef1c
Amanelle27 <amanellecorp@gmail.com>
2025-01-29 10:44:54 +0500

    Added the second task of classes

---
+++ lab03/classes/02task.py
@@ -0,0 +1,18 @@
+class Shape:
+    def __init__(self):
+        self.area = 0
+    def area(self):
+        return self.area
+    
+class Square(Shape):
+    def __init__(self, length):
+        super().__init__(self)
+        self.length = length
+
+    def area(self):
+        self.area = self.length * self.length
+        return self.area
+    
+
+
+        
