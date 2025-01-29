bd5d5033b3d9c017c118a87cf79c23e29feb93da
Amanelle27 <amanellecorp@gmail.com>
2025-01-29 09:11:06 +0500

    Added the fourth task

--- lab03/functions1/04task.py
+++ lab03/functions1/04task.py
@@ -1,2 +1,18 @@
-def filter_prime(*inputlist):
-    
+def isXprime(x):
+    if x < 2:
+        return False
+    for i in range(1, x):
+        if i != 1 and x%i == 0:
+            return False
+    return True
+
+def filter_prime(inputList):
+    resList = []
+    for x in inputList:
+        if isXprime(int(x)):
+            resList.append(x)
+    return resList
+
+listIn = list(input().split())
+print(filter_prime(listIn))
+
