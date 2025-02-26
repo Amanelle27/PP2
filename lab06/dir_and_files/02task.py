f63289c2576bad4caa710e5533852e98b794d0c9
Amanelle27 <amanellecorp@gmail.com>
2025-02-26 09:41:18 +0500

    Added the second dir-and-files task

---
+++ lab06/dir_and_files/02task.py
@@ -0,0 +1,9 @@
+import os
+
+path = r'C:\Users\user\Desktop\calculus 1'
+print("Our path:", path)
+
+print("Does this path exist?", os.access(path, os.F_OK))
+print("Can we read it?", os.access(path, os.R_OK))
+print("Can we edit it?", os.access(path, os.W_OK))
+print("Can we execute it?", os.access(path, os.X_OK))
